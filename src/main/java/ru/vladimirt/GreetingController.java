4d40b168e423d8b46a257613487f8490e758ea90
Vladimir Tikaev <vladimirt.215@gmail.com>
2019-12-07 21:48:48 +0300

    Add new methods in controller for work with Message entity

--- src/main/java/ru/vladimirt/GreetingController.java
+++ src/main/java/ru/vladimirt/GreetingController.java
@@ -1,14 +1,19 @@
 package ru.vladimirt;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import ru.vladimirt.domain.Message;
+import ru.vladimirt.repositories.IMessageRepository;
 
 import java.util.Map;
 
 @Controller
 public class GreetingController {
+    @Autowired
+    private IMessageRepository messageRepository;
 
     //Этот метод обрабатывает гет запросы по адресу /greeting
     //В параметрах метода мы передаем параметр name, который соответсвует name
@@ -27,7 +32,35 @@ public class GreetingController {
 
     @GetMapping
     public String main(Map<String, Object> model){
-        model.put("some","Hello Vladimir. It is Main page");
+        Iterable<Message> messages = messageRepository.findAll();
+        model.put("messages",messages);
+        return "main";
+    }
+
+    @PostMapping
+    public String addMessage(@RequestParam String text, @RequestParam String tag, Map<String, Object> model){
+        Message message = new Message();
+        message.setText(text);
+        message.setTag(tag);
+        messageRepository.save(message);
+
+        Iterable<Message> messages = messageRepository.findAll();
+        model.put("messages",messages);
+        return "main";
+    }
+
+    @PostMapping("filter")
+    public String showByFilter(@RequestParam String filter,
+                               Map<String, Object> model){
+
+        Iterable<Message> messages;
+        if(filter != null && !filter.isEmpty()){
+            messages = messageRepository.findByTextContaining(filter);
+        }else {
+            messages = messageRepository.findAll();
+        }
+
+        model.put("messages",messages);
         return "main";
     }
 }
