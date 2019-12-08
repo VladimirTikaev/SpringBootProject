a26a0baab5fbe28a8a0fdc1f24d6f9165a00cf70
Vladimir Tikaev <vladimirt.215@gmail.com>
2019-12-08 11:40:51 +0300

    Change Role enum for user class

--- src/main/java/ru/vladimirt/domain/Role.java
+++ src/main/java/ru/vladimirt/domain/Role.java
@@ -1,5 +1,12 @@
 package ru.vladimirt.domain;
 
-public enum Role {
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
     USER;
+
+    @Override
+    public String getAuthority() {
+        return name(); //Строковое значение элементов перечисления
+    }
 }
