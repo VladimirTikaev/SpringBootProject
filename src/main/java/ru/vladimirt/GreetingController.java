3b1f491ed53c4cbbdd3eb316a62a8cb82da6de9a
Vladimir Tikaev <vladimirt.215@gmail.com>
2019-12-08 08:51:51 +0300

    Change mapping. Now greeting is start page

--- src/main/java/ru/vladimirt/GreetingController.java
+++ src/main/java/ru/vladimirt/GreetingController.java
@@ -21,23 +21,19 @@ public class GreetingController {
     // то подставится значение по умаолчнию World
 //    Значение параметра name добавляется в объект Model,
 //    что в конечном итоге делает его доступным для шаблона представления.
-    @GetMapping("/greeting")
-    public String greeting(
-            @RequestParam(name="name", required = false, defaultValue = "World") String name,
-            Map<String, Object> model)
-    {
-        model.put("name", name);
+    @GetMapping("/")
+    public String greeting(Map<String, Object> model){
         return "greeting";
     }
 
-    @GetMapping
+    @GetMapping("/main")
     public String main(Map<String, Object> model){
         Iterable<Message> messages = messageRepository.findAll();
         model.put("messages",messages);
         return "main";
     }
 
-    @PostMapping
+    @PostMapping("/main")
     public String addMessage(@RequestParam String text, @RequestParam String tag, Map<String, Object> model){
         Message message = new Message();
         message.setText(text);
