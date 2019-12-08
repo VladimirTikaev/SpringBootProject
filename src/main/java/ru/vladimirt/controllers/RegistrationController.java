94e4934400545c1d6565597a025ee332d195d071
Vladimir Tikaev <vladimirt.215@gmail.com>
2019-12-08 10:56:22 +0300

    Add Registration controller

---
+++ src/main/java/ru/vladimirt/controllers/RegistrationController.java
@@ -0,0 +1,39 @@
+package ru.vladimirt.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import ru.vladimirt.domain.Role;
+import ru.vladimirt.domain.User;
+import ru.vladimirt.repositories.IUserRepository;
+
+import java.util.Collections;
+import java.util.Map;
+
+@Controller
+public class RegistrationController {
+
+    @Autowired
+    private IUserRepository userRepository;
+
+    @GetMapping("/registration")
+    public String registration(){
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String addUser(User user, Map<String, Object> model){
+        User userFromDb = userRepository.findByUsername(user.getUsername());
+
+        if(userFromDb != null){
+            model.put("message", "User exists!");
+            return "registration";
+        }
+
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.USER));
+        userRepository.save(user);
+        return "redirect:/login";
+    }
+}
