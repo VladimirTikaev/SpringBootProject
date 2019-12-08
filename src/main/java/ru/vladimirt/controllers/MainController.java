cd9d39c29b1e0568c8fe0270226d7a65d2397faa
Vladimir Tikaev <vladimirt.215@gmail.com>
2019-12-08 12:50:01 +0300

    Fix filter in MainController

--- src/main/java/ru/vladimirt/controllers/MainController.java
+++ src/main/java/ru/vladimirt/controllers/MainController.java
@@ -3,6 +3,7 @@ package ru.vladimirt.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -29,9 +30,17 @@ public class MainController {
     }
 
     @GetMapping("/main")
-    public String main(Map<String, Object> model){
+    public String main(@RequestParam(required = false, defaultValue = "") String filter, Model model){
         Iterable<Message> messages = messageRepository.findAll();
-        model.put("messages",messages);
+
+        if(filter != null && !filter.isEmpty()){
+            messages = messageRepository.findByTextContaining(filter);
+        }else {
+            messages = messageRepository.findAll();
+        }
+
+        model.addAttribute("messages",messages);
+        model.addAttribute("filter", filter);
         return "main";
     }
 
@@ -49,18 +58,5 @@ public class MainController {
         return "main";
     }
 
-    @PostMapping("filter")
-    public String showByFilter(@RequestParam String filter,
-                               Map<String, Object> model){
 
-        Iterable<Message> messages;
-        if(filter != null && !filter.isEmpty()){
-            messages = messageRepository.findByTextContaining(filter);
-        }else {
-            messages = messageRepository.findAll();
-        }
-
-        model.put("messages",messages);
-        return "main";
-    }
 }
