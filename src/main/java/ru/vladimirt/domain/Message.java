3521f4031b5e7e36dcf78833122f055892f8f484
Vladimir Tikaev <vladimirt.215@gmail.com>
2019-12-08 11:43:11 +0300

    Add new field author and connection @ManyToOne with User for class message

--- src/main/java/ru/vladimirt/domain/Message.java
+++ src/main/java/ru/vladimirt/domain/Message.java
@@ -9,11 +9,33 @@ public class Message {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @Column
     private String text;
 
-    @Column
     private String tag;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User author;
+
+    public Message() {
+    }
+
+    public Message(String text, String tag, User author) {
+        this.text = text;
+        this.tag = tag;
+        this.author = author;
+    }
+
+    public String getAuthorName(){
+        return author != null? author.getUsername() : "<none>";
+    }
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
 
     public String getText() {
         return text;
