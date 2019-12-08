3a05f12e82de93fcc3110d7c1c2cea36f150bd95
Vladimir Tikaev <vladimirt.215@gmail.com>
2019-12-08 08:58:47 +0300

    Create User class

---
+++ src/main/java/ru/vladimirt/domain/User.java
@@ -0,0 +1,70 @@
+package ru.vladimirt.domain;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "usr")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String userName;
+
+    private String password;
+
+    private boolean active;
+
+    //Не совсем понятно для чего эта аннотация.... Для работы с коллекциями
+    //feth - способ подгрузки EAGER - подгружает сразу все при запросе пользователя
+    // LAZY- подгружает только при обращении к полю
+    @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
+    // Указывается что роли будут хранится в отдельной таблице, которую мы не описывали ранее
+    // а также поле, по которому таблицы связаны
+    @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
+    // Говорим, что храним перечисление в виде строк
+    @Enumerated(EnumType.STRING)
+    private Set<Role> roles;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+}
