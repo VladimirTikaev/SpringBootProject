d0ba05a7b39d77b9a583f7f603498ecba89f2734
Vladimir Tikaev <vladimirt.215@gmail.com>
2019-12-10 17:11:25 +0300

    Add configuration for static folder and for work with files

--- src/main/java/ru/vladimirt/config/MvcConfig.java
+++ src/main/java/ru/vladimirt/config/MvcConfig.java
@@ -1,14 +1,28 @@
 package ru.vladimirt.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
+    @Value("${upload.path}") //Это спел. Здесь он ищет свойство upload.path и вставляет в переменную
+    private String uploadPath;
+
     public void addViewControllers(ViewControllerRegistry registry) {
 
         registry.addViewController("/login").setViewName("login");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //Любое обращение к серверу по пути img + что-то еще будет перенаправлять все запросы по указанному пути
+        registry.addResourceHandler("/img/**")
+                .addResourceLocations("file:///" + uploadPath + "/"); //Ищет в файловой системе
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");//Ищет в корне проекта
     }
 }
