d964959403a3d70823c9c9cf5fb12940736692e7
Vladimir Tikaev <vladimirt.215@gmail.com>
2019-12-10 17:10:25 +0300

    Add work with file in MessageController

--- src/main/java/ru/vladimirt/controllers/MainController.java
+++ src/main/java/ru/vladimirt/controllers/MainController.java
@@ -1,22 +1,30 @@
 package ru.vladimirt.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import ru.vladimirt.domain.Message;
 import ru.vladimirt.domain.User;
 import ru.vladimirt.repositories.IMessageRepository;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
+import java.util.UUID;
 
 @Controller
 public class MainController {
     @Autowired
     private IMessageRepository messageRepository;
+
+    @Value("${upload.path}") //Это спел. Здесь он ищет свойство upload.path и вставляет в переменную
+    private String uploadPath;
 
     //Этот метод обрабатывает гет запросы по адресу /greeting
     //В параметрах метода мы передаем параметр name, который соответсвует name
@@ -49,8 +57,26 @@ public class MainController {
             @AuthenticationPrincipal User user,
             @RequestParam String text,
             @RequestParam String tag,
-            Map<String, Object> model){
+            @RequestParam("file") MultipartFile file,
+            Map<String, Object> model) throws IOException {
         Message message = new Message(text, tag, user);
+
+        if(file != null && !file.getOriginalFilename().isEmpty()){
+
+            File uploadDir = new File(uploadPath);
+            if(!uploadDir.exists()){ //Если такой переменной не существует, то мы ее создадим
+                uploadDir.mkdir();
+            }
+
+            // Создаем уникальный идентификатор, чтобы небыло коллизий в именах файлов
+            String uuidFile = UUID.randomUUID().toString();
+
+            String resultFileName = uuidFile + "." + file.getOriginalFilename();
+
+            file.transferTo(new File(uploadPath + "/" + resultFileName)); // Создаем файл с уникальным именем
+            message.setFileName(resultFileName);
+        }
+
         messageRepository.save(message);
 
         Iterable<Message> messages = messageRepository.findAll();
