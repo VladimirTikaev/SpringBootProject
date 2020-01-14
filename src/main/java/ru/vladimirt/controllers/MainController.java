dacfaf5c4ccf3aebcf901746860a707e0dce1746
Vladimir Tikaev <vladimirt.215@gmail.com>
2020-01-14 14:44:34 +0300

    Change MainController for validate fields

--- src/main/java/ru/vladimirt/controllers/MainController.java
+++ src/main/java/ru/vladimirt/controllers/MainController.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,10 +15,12 @@ import ru.vladimirt.domain.Message;
 import ru.vladimirt.domain.User;
 import ru.vladimirt.repositories.IMessageRepository;
 
+import javax.validation.Valid;
 import java.io.File;
 import java.io.IOException;
 import java.util.Map;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Controller
 public class MainController {
@@ -55,32 +59,46 @@ public class MainController {
     @PostMapping("/main")
     public String addMessage(
             @AuthenticationPrincipal User user,
-            @RequestParam String text,
-            @RequestParam String tag,
-            @RequestParam("file") MultipartFile file,
-            Map<String, Object> model) throws IOException {
-        Message message = new Message(text, tag, user);
+            @Valid Message message, //Вместо 2-ух параметров используем 1 - просто класс. Также теперь можно добавить валидацию
+            BindingResult bindingResult, // Список аргументов и сообщений ощибок валидаций. Всегда должен идти перед model
+            Model model,
+            @RequestParam("file") MultipartFile file)
+             throws IOException {
 
-        if(file != null && !file.getOriginalFilename().isEmpty()){
+        message.setAuthor(user);
 
-            File uploadDir = new File(uploadPath);
-            if(!uploadDir.exists()){ //Если такой переменной не существует, то мы ее создадим
-                uploadDir.mkdir();
+
+        //Проверка ошибок валидации
+        if(bindingResult.hasErrors()){
+            Map<String, String> errorsMap = ControllerUtils.getErrors(bindingResult);
+
+            model.mergeAttributes(errorsMap);
+            model.addAttribute("message", message);
+
+        }else { // Если ошибок нет, то добавляем в бд
+
+            if (file != null && !file.getOriginalFilename().isEmpty()) {
+
+                File uploadDir = new File(uploadPath);
+                if (!uploadDir.exists()) { //Если такой переменной не существует, то мы ее создадим
+                    uploadDir.mkdir();
+                }
+
+                // Создаем уникальный идентификатор, чтобы небыло коллизий в именах файлов
+                String uuidFile = UUID.randomUUID().toString();
+
+                String resultFileName = uuidFile + "." + file.getOriginalFilename();
+
+                file.transferTo(new File(uploadPath + "/" + resultFileName)); // Создаем файл с уникальным именем
+                message.setFileName(resultFileName);
             }
 
-            // Создаем уникальный идентификатор, чтобы небыло коллизий в именах файлов
-            String uuidFile = UUID.randomUUID().toString();
-
-            String resultFileName = uuidFile + "." + file.getOriginalFilename();
-
-            file.transferTo(new File(uploadPath + "/" + resultFileName)); // Создаем файл с уникальным именем
-            message.setFileName(resultFileName);
+            model.addAttribute("message", null);
+            messageRepository.save(message);
         }
 
-        messageRepository.save(message);
-
         Iterable<Message> messages = messageRepository.findAll();
-        model.put("messages",messages);
+        model.addAttribute("messages",messages);
         return "main";
     }
 
