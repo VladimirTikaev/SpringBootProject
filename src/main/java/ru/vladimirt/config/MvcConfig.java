21a7e4b381e2691c106fe5d3024ae2d0e06a86d4
Vladimir Tikaev <vladimirt.215@gmail.com>
2020-01-14 15:44:58 +0300

    Create RestTemplate bean in MvcConfig

--- src/main/java/ru/vladimirt/config/MvcConfig.java
+++ src/main/java/ru/vladimirt/config/MvcConfig.java
@@ -1,7 +1,9 @@
 package ru.vladimirt.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,6 +13,11 @@ public class MvcConfig implements WebMvcConfigurer {
 
     @Value("${upload.path}") //Это спел. Здесь он ищет свойство upload.path и вставляет в переменную
     private String uploadPath;
+
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return  new RestTemplate();
+    }
 
     public void addViewControllers(ViewControllerRegistry registry) {
 
