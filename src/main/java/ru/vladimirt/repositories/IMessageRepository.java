32a8f404338846e781aeeedde8ad39bee7010ed9
Vladimir Tikaev <vladimirt.215@gmail.com>
2020-01-16 14:42:57 +0300

    Add method findByAuthor int IMessageRepository for correct showing pagination on UserMessages page

--- src/main/java/ru/vladimirt/repositories/IMessageRepository.java
+++ src/main/java/ru/vladimirt/repositories/IMessageRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import ru.vladimirt.domain.Message;
+import ru.vladimirt.domain.User;
 
 import java.util.List;
 
@@ -12,6 +13,7 @@ public interface IMessageRepository extends CrudRepository<Message, Long> {
 
 
     Page<Message> findByTagContaining(String filter, Pageable pageable);
+    Page<Message> findByAuthor(User user, Pageable pageable);
     Page<Message> findAll(Pageable pageable);
 
 
