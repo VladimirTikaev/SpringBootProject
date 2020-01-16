9a83adbd4532fcaa3cbe24a26ddef1775914f4fa
Vladimir Tikaev <vladimirt.215@gmail.com>
2020-01-16 20:46:38 +0300

    Change message service code for correct using IMessageRepository's methods

--- src/main/java/ru/vladimirt/services/MessageService.java
+++ src/main/java/ru/vladimirt/services/MessageService.java
@@ -6,7 +6,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import ru.vladimirt.domain.Message;
 import ru.vladimirt.domain.User;
+import ru.vladimirt.domain.dto.MessageDto;
 import ru.vladimirt.repositories.IMessageRepository;
+
+import javax.persistence.EntityManager;
 
 @Service
 public class MessageService {
@@ -14,17 +17,17 @@ public class MessageService {
     @Autowired
     private IMessageRepository messageRepository;
 
-    public Page<Message> messageList(Pageable pageable, String filter){
+    public Page<MessageDto> messageList(Pageable pageable, String filter, User user){
         if(filter != null && !filter.isEmpty()){
-            return messageRepository.findByTagContaining(filter, pageable);
+            return messageRepository.findByTag(filter, pageable, user);
         }else {
-            return messageRepository.findAll(pageable);
+            return messageRepository.findAll(pageable, user);
         }
     }
 
-    public Page<Message> messageListForUser(Pageable pageable, User author) {
+    public Page<MessageDto> messageListForUser(Pageable pageable, User currentUser, User author) {
 
-        return messageRepository.findByAuthor(author, pageable);
+        return messageRepository.findByUser(pageable, author, currentUser );
 
     }
 }
