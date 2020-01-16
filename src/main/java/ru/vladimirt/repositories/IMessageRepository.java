d3eb993814080ffb8008de58a7bd847761eff630
Vladimir Tikaev <vladimirt.215@gmail.com>
2020-01-16 20:45:48 +0300

    Add HQL query to repository's methods for using likes

--- src/main/java/ru/vladimirt/repositories/IMessageRepository.java
+++ src/main/java/ru/vladimirt/repositories/IMessageRepository.java
@@ -2,19 +2,46 @@ package ru.vladimirt.repositories;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import ru.vladimirt.domain.Message;
 import ru.vladimirt.domain.User;
+import ru.vladimirt.domain.dto.MessageDto;
 
 import java.util.List;
 
 //Спринг автоматически создает бин
 public interface IMessageRepository extends CrudRepository<Message, Long> {
 
+    @Query("select new ru.vladimirt.domain.dto.MessageDto(" +
+            "   m, " +
+            "   count(ml), " +
+            "   sum(case when ml = :user then 1 else 0 end) > 0" +
+            ") " +
+            "from Message m left join m.likes ml " +
+            "where m.tag = :tag " +
+            "group by m")
+    Page<MessageDto> findByTag(@Param("tag")  String tag, Pageable pageable, @Param("user") User user);
 
-    Page<Message> findByTagContaining(String filter, Pageable pageable);
-    Page<Message> findByAuthor(User user, Pageable pageable);
-    Page<Message> findAll(Pageable pageable);
+    @Query("select new  ru.vladimirt.domain.dto.MessageDto(" +
+            "   m, " +
+            "   count(ml), " +
+            "   sum(case when ml = :user then 1 else 0 end) > 0" +
+            ") " +
+            "from Message m left join m.likes ml " +
+            "where m.author = :author " +
+            "group by m")
+    Page<MessageDto> findByUser(Pageable pageable, @Param("author") User author, @Param("user") User user);
+
+    @Query("select new  ru.vladimirt.domain.dto.MessageDto(" +
+            "   m, " +
+            "   count(ml), " +
+            "   sum(case when ml = :user then 1 else 0 end) > 0" +
+            ") " +
+            "from Message m left join m.likes ml " +
+            "group by m")
+    Page<MessageDto> findAll(Pageable pageable, @Param("user") User user);
 
 
 }
